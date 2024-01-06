5888e0af1011b2f663bb03b255c553ba3d078664
tiiimezombie <tiiimezombie@gmail.com>
2024-01-06 16:36:11 -0500

    initial commit

---
+++ InternOnboarding/Patcher.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using GameNetcodeStuff;
+using HarmonyLib;
+using BepInEx.Logging;
+using System.Linq;
+using System;
+
+namespace TZ.InternOnboarding
+{
+    internal class OverallPatch
+    {
+        internal static Terminal terminal;
+        internal static RoundManager roundManager;
+    }
+
+    [HarmonyPatch(typeof(Terminal))]
+    internal class PatchedTerminal
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void GrabDetails(ref Terminal __instance)
+        {
+            OverallPatch.terminal = __instance;
+        }
+    }
+
+    [HarmonyPatch(typeof(TimeOfDay))]
+	internal class PatchedTimeOfDay
+	{
+        static internal ManualLogSource _logSource;
+
+		[HarmonyPatch("SetBuyingRateForDay")]
+		[HarmonyPostfix]
+        static void GiveItems(ref TimeOfDay __instance) // ref QuotaSettings __quotaSettings
+        {
+            _logSource = BepInEx.Logging.Logger.CreateLogSource(InternOnboardingInfo.PLUGIN_GUID);
+
+            // need to make this only trigger once , ie the first day or when quota fulfilled is 0
+            _logSource.LogInfo("i wanna give items");
+            if ((int)__instance.quotaFulfilled > 0) return;
+            _logSource.LogInfo("quota not fulfilled");
+            if (CalculateLootValue() > 0) return;
+            _logSource.LogInfo("scrap not collected");
+
+            //_logSource.LogInfo((int)__instance.timeUntilDeadline);
+            //_logSource.LogInfo((int)__instance.totalTime);
+            //_logSource.LogInfo((int)Mathf.Floor(__instance.timeUntilDeadline / __instance.totalTime)); //__quotaSettings.deadlineDaysAmount
+
+            OverallPatch.terminal.orderedItemsFromTerminal.Add(0);
+            OverallPatch.terminal.orderedItemsFromTerminal.Add(0);
+            OverallPatch.terminal.orderedItemsFromTerminal.Add(1);
+        }
+
+        /// <summary>
+        /// https://github.com/tinyhoot/ShipLoot/blob/main/ShipLoot/Patches/HudManagerPatcher.cs
+        /// </summary>
+        private static float CalculateLootValue()
+        {
+            GameObject ship = GameObject.Find("/Environment/HangarShip");
+            var loot = ship.GetComponentsInChildren<GrabbableObject>()
+                .Where(obj => obj.itemProperties.isScrap).ToList();
+
+            return loot.Sum(scrap => scrap.scrapValue);
+        }
+    }
+}
---
+++ InternOnboarding/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+using HarmonyLib;
+
+namespace TZ.InternOnboarding
+{
+    public class InternOnboardingInfo : PluginInfo
+    {
+        public const string PLUGIN_GUID = "TZ.InternOnboarding";
+        public const string PLUGIN_NAME = "Intern Onboarding";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+
+    /// <summary>
+    /// https://www.reddit.com/r/lethalcompany_mods/comments/18refsw/tutorial_creating_lethal_company_mods_with_c/
+    /// https://www.youtube.com/watch?v=4Q7Zp5K2ywI
+    /// </summary>
+    [BepInPlugin(InternOnboardingInfo.PLUGIN_GUID, InternOnboardingInfo.PLUGIN_NAME, InternOnboardingInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony _harmony = new Harmony(InternOnboardingInfo.PLUGIN_GUID);
+        internal ManualLogSource _logSource;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {InternOnboardingInfo.PLUGIN_GUID} is loaded!");
+
+            _logSource = BepInEx.Logging.Logger.CreateLogSource(InternOnboardingInfo.PLUGIN_GUID);
+            _harmony.PatchAll(typeof(Plugin));
+
+            _harmony.PatchAll(typeof(PatchedTerminal));
+            _harmony.PatchAll(typeof(PatchedTimeOfDay));
+        }
+    }
+}
